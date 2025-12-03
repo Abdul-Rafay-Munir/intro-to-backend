745aadc04d30e6b08f222f124a9a1c4dad401523
Abdul-Rafay-Munir <munirabdulrafay@gmail.com>
2025-12-03 22:41:58 +0500

    created routes

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
-import dotenv from "dotenv"
-import connectDB from "./config/database.js"
-import app from "./app.js"
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
+import app from "./app.js";
 dotenv.config({
-    path:'./.env'
-})
+  path: "./.env",
+});
 
-const startServer=async () =>{
-    try {
-        await connectDB()
+const startServer = async () => {
+  try {
+    await connectDB();
 
-        app.on("error",(error)=>{
-            console.log("Error",error)
-            throw error
-        })
+    app.on("error", (error) => {
+      console.log("Error", error);
+      throw error;
+    });
 
-        app.listen(process.env.PORT || 8000,()=>{
-            console.log(`Server is running on port :
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port :
                 ${process.env.PORT}`);
-            
-        })
-    } catch (error) {
-        console.log("MongoDB connection failed!!",error);
-    }
-}
+    });
+  } catch (error) {
+    console.log("MongoDB connection failed!!", error);
+  }
+};
 
-startServer()
+startServer();
