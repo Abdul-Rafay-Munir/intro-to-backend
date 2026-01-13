b175c9210090e8e07c27d4c672435ff24aaf4470
Abdul-Rafay-Munir <munirabdulrafay@gmail.com>
2026-01-13 19:53:10 +0500

    created post router & controller

---
+++ src/controllers/post.controller.js
@@ -0,0 +1,23 @@
+import { Post } from "../models/post.model";
+
+// Create a Post
+
+const createPost = async (req, res) => {
+  try {
+    const { name, description, age } = req.body;
+
+    if (!name || !description || !age) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const post = await Post({ name, description, age });
+
+    res.status(201).json({ message: "Post created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server error", error });
+  }
+};
+
+export{
+    createPost
+}
---
+++ src/routes/post.route.js
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { createPost } from "../controllers/post.controller";
+
+const router = Router();
+
+router.route("/create").post(createPost);
